fed4376b594962b64fe370f0f128dd4d6cc90d35
AdarshJagadish <jagadishadarsh2003@gmail.com>
2024-12-07 13:28:32 +0530

    initial commit

--- todo/app/models.py
+++ todo/app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class lists(models.Model):
+    tasks=models.TextField()
+    date=models.DateField(auto_now_add=True,null=True)
--- todo/app/views.py
+++ todo/app/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import *
+
+def index1(request):
+    tasks=lists.objects.all()
+    if request.method == 'POST':
+        tasks=request.POST['task']
+        data=lists.objects.create(tasks=tasks)
+        data.save()
+    return render(request,'index1.html',{'tasks':tasks})
+
+def index2(request):
+    tasks = lists.objects.all()
+    return render(request,'index2.html',)
 
 # Create your views here.
